e8f6e9cb03c1b7c346e82bc78808fa2aa85ddb90
Andrey Seleznov <andrewaka39@gmail.com>
2017-03-21 13:45:54 +0000

    initial spec for post request

--- app/api/shortener/api.rb
+++ app/api/shortener/api.rb
@@ -1,5 +1,14 @@
 module Shortener
   class API < Grape::API
+    format :json
 
+    desc 'Create short url'
+    params do
+      requires :url, type: String, desc: 'URL for shortening'
+    end
+    post '/' do
+      status 201
+      { short_url: '/abc123', url: params[:url] }
+    end
   end
 end
--- spec/api/shortener/api_spec.rb
+++ spec/api/shortener/api_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe Shortener::API, type: :request do
-  it 'true returns true' do
-    expect(true).to eq(true)
+  describe 'POST /' do
+    it 'creates short url' do
+      post "/",  params: { url: 'http://www.farmdrop.com'  }
+      expect(response.status).to eq(201)
+      p response.body
+    end
   end
 end
