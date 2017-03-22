f5326e5c1b549d565c9208d6c6a25db4db69ce64
Andrey Seleznov <andrewaka39@gmail.com>
2017-03-22 14:18:23 +0000

    spec for generate shortener_service & shortener lib cleaning

--- lib/shortener_lib.rb
+++ lib/shortener_lib.rb
@@ -1,18 +1,5 @@
 module ShortenerLib
   ALPHABET ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./:".split(//)
-  URL_REGEX = /\b((https?):\/\/([\w\.-]+[\.]\w+)?)([-A-Z0-9+&@#\/%?=~_|$!:,.;]*[-A-Z0-9+&@#\/%=~_|$])?/i
-
-  def self.generate(url)
-    return unless URL_REGEX.match(url)
-
-    decode(URL_REGEX.match(url)[3])
-  end
-
-  protected
-
-  def self.decode(url)
-    bijective_decode(url)
-  end
 
   def self.bijective_encode(i)
     # from http://refactormycode.com/codes/125-base-62-encoding
@@ -31,10 +18,8 @@ module ShortenerLib
     # based on base2dec() in Tcl translation
     # at http://rosettacode.org/wiki/Non-decimal_radices/Convert#Ruby
     i = 0
-
     base = ALPHABET.length
     string.each_char { |char| i = i * base + ALPHABET.index(char) }
-
     i
   end
 end
--- spec/app/services/shortener_service_spec.rb
+++ spec/app/services/shortener_service_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 describe ShortenerService do
   let(:service) { ShortenerService.new(url) }
 
+  describe '#generate' do
+    let(:url) { 'http://www.farmdrop.com' }
+
+    it 'creates a new Url record' do
+      expect{ service.generate }.to change{ Url.count }.from(0).to(1)
+    end
+
+    it 'creates a valid record' do
+      service.generate
+      expect(Url.last.url).to eq(url)
+      p Url.last
+    end
+  end
+
   context 'url without protocol' do
     let(:url) { 'www.farmdrop.com' }
     let(:url_with_protocol) { 'http://www.farmdrop.com' }
